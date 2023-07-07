f14c3d593415f0667a420bd0d9e43614d2eaf2b8
Glenn Halls <gjhalls@gmail.com>
2023-07-07 17:13:01 +1000

    Add: navigationType modifier

--- app/src/main/java/com/example/wowinfo/ui/WowInfoApp.kt
+++ app/src/main/java/com/example/wowinfo/ui/WowInfoApp.kt
@@ -1,11 +1,32 @@
 package com.example.wowinfo.ui
 
+import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import com.example.wowinfo.ui.util.WowInfoNavigationType
 
 @Composable
 fun WowInfoApp(
     windowSize: WindowSizeClass,
     modifier: Modifier = Modifier
-) {}
+) {
+    // Create an instance of WowInfoViewModel
+    val viewModel = WowInfoViewModel()
+    // Create an observable reference to uiState
+    val uiState by viewModel.uiState.collectAsState()
+
+    // Define navigation type based on WindowSize dimensions
+    val navigationType: WowInfoNavigationType =
+        if (windowSize.widthSizeClass == WindowWidthSizeClass.Compact) {
+            WowInfoNavigationType.BOTTOM_BAR
+        } else when (windowSize.heightSizeClass) {
+            WindowHeightSizeClass.Compact -> WowInfoNavigationType.BOTTOM_BAR
+            WindowHeightSizeClass.Medium -> WowInfoNavigationType.NAVIGATION_DRAWER
+            WindowHeightSizeClass.Expanded -> WowInfoNavigationType.NAVIGATION_RAIL
+            else -> WowInfoNavigationType.BOTTOM_BAR
+        }
+}
---
+++ app/src/main/java/com/example/wowinfo/ui/util/WindowStateUtils.kt
@@ -0,0 +1,9 @@
+package com.example.wowinfo.ui.util
+
+enum class WowInfoNavigationType {
+    BOTTOM_BAR, NAVIGATION_RAIL, NAVIGATION_DRAWER
+}
+
+enum class WowInfoContentLayout {
+    LIST_ONLY, DETAILS_ONLY, LIST_AND_DETAILS
+}

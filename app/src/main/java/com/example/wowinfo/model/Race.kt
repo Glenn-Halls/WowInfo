4bf565c4279fa41d437da331a2c2f63c728bf612
Glenn Halls <gjhalls@gmail.com>
2023-07-10 15:20:22 +1000

    Add: Race class

---
+++ app/src/main/java/com/example/wowinfo/model/Race.kt
@@ -0,0 +1,11 @@
+package com.example.wowinfo.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Race(
+    @StringRes val name: Int,
+    val faction: Faction,
+    @DrawableRes val crest: Int,
+    @StringRes val description: Int,
+)
